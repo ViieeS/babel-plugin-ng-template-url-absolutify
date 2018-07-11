d7ea81cdcf0efc870aabc9b4046df8c3a087e066
ViES <vies@list.ru>
2018-07-11 11:45:33 +0300

    Fix absolute path transformations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import {resolve, dirname} from 'path';
 
+const isValidUrl = string => {
+    try {
+        new URL(string);
+        return true;
+    } catch (_) {
+        return false;
+    }
+};
+
 export default function ({types: t}) {
     return {
         visitor: {
@@ -9,15 +18,15 @@ export default function ({types: t}) {
                     if (t.isStringLiteral(path.node.value)) {
 
                         const {baseDir, baseUrl} = state.opts;
-
                         const dir = dirname(state.file.opts.filename);
                         const templateUrl = path.node.value.value;
 
-                        const absolutePath = resolve(dir, templateUrl);
+                        if (!isValidUrl(templateUrl)) {
+                            const absolutePath = resolve(dir, templateUrl);
+                            const url = baseUrl + absolutePath.replace(resolve(baseDir), '');
 
-                        const url = baseUrl + absolutePath.replace(resolve(baseDir), '');
-
-                        path.node.value = t.stringLiteral(url);
+                            path.node.value = t.stringLiteral(url);
+                        }
                     }
             }
         }
